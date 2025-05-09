c593573e400b01c8c28bdb70fa16c50042cbbd6b
Edra Prasetio <edraadrianp@gmail.com>
2025-05-09 13:57:25 -0700

    first commit

---
+++ WarCardGame/WarCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardGameApp.swift
+//  WarCardGame
+//
+//  Created by Edra Prasetio on 08/05/25.
+//
+
+import SwiftUI
+
+@main
+struct WarCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
